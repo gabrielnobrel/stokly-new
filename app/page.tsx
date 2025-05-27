4513110f4d06f558bc0ad9db9382b5041e7f8a44
gabrielnobrel <gabriel_nobresantos@hotmail.com>
2025-05-27 14:22:44 -0300

    refactor: add header component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,24 @@
+import Header, {
+  HeaderLeft,
+  HeaderRight,
+  HeaderSubtitle,
+  HeaderTitle,
+} from "./_components/header";
+import { Button } from "./_components/ui/button";
+
 const Home = () => {
   return (
-    <div>
-      <h1>home page</h1>
+    <div className="m-8 w-full space-y-8 rounded-lg bg-white p-8">
+      <Header>
+        <HeaderLeft>
+          <HeaderTitle>Dashboard</HeaderTitle>
+          <HeaderSubtitle>Visão geral dos dados</HeaderSubtitle>
+        </HeaderLeft>
+
+        <HeaderRight>
+          <Button>Adicionar</Button>
+        </HeaderRight>
+      </Header>
     </div>
   );
 };
