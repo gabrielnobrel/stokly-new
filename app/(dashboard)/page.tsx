acd1d067ee5552f086fd761f89eb794154a99af3
gabrielnobrel <gabriel_nobresantos@hotmail.com>
2025-05-28 14:03:24 -0300

    feta: add initial  revenue chart

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -17,13 +17,20 @@ import {
 } from "./_components/summary-card";
 import { getDashboard } from "../_data-access/dashboard/get-dashboard";
 import { formatCurrency } from "../_helpers/currency";
+import RevenueChart from "./_components/revenue-chart";
 
 const Home = async () => {
-  const { totalRevenue, todayRevenue, totalSales, totalStock, totalProducts } =
-    await getDashboard();
+  const {
+    totalRevenue,
+    todayRevenue,
+    totalSales,
+    totalStock,
+    totalLast14DaysRevenue,
+    totalProducts,
+  } = await getDashboard();
 
   return (
-    <div className="m-8 w-full space-y-8 rounded-lg">
+    <div className="m-8 w-full space-y-8 rounded-lg flex flex-col">
       <Header>
         <HeaderLeft>
           <HeaderTitle>Dashboard</HeaderTitle>
@@ -73,6 +80,12 @@ const Home = async () => {
           <SummaryCardTitle>Produtos</SummaryCardTitle>
           <SummaryCardValue>{totalProducts}</SummaryCardValue>
         </SummaryCard>
+      </div>
+
+      <div className="flex h-full flex-col overflow-hidden rounded-xl bg-white p-6">
+        <p className="text-lg font-semibold text-slate-900">Receita</p>
+        <p className="text-slate-400">Últimos 14 dias</p>
+        <RevenueChart data={totalLast14DaysRevenue} />
       </div>
     </div>
   );
--- app/_data-access/dashboard/get-dashboard.ts
+++ app/_data-access/dashboard/get-dashboard.ts
@@ -1,4 +1,12 @@
+import "server-only";
+
 import { db } from "@/app/_lib/prisma";
+import dayjs from "dayjs";
+
+export interface DayTotalRevenue {
+  day: string;
+  totalRevenue: number;
+}
 
 interface DashboardDto {
   totalRevenue: number;
@@ -6,9 +14,39 @@ interface DashboardDto {
   totalSales: number;
   totalStock: number;
   totalProducts: number;
+  totalLast14DaysRevenue: DayTotalRevenue[];
 }
 
 export const getDashboard = async (): Promise<DashboardDto> => {
+  const today = dayjs().endOf("day").toDate();
+  const last14Days = [13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0].map(
+    (day) => {
+      return dayjs(today).subtract(day, "day");
+    }
+  );
+
+  const totalLast14DaysRevenue: DayTotalRevenue[] = [];
+  for (const day of last14Days) {
+    const dayTotalRevenue = await db.$queryRawUnsafe<
+      {
+        totalRevenue: number;
+      }[]
+    >(
+      `
+        SELECT SUM("unitPrice" * "quantity") as "totalRevenue"
+        FROM "SaleProduct"
+        WHERE "createdAt" >= $1 AND "createdAt" <= $2
+      `,
+      day.startOf("day").toDate(),
+      day.endOf("day").toDate()
+    );
+
+    totalLast14DaysRevenue.push({
+      day: day.format("DD/MM"),
+      totalRevenue: dayTotalRevenue[0]?.totalRevenue ?? 0,
+    });
+  }
+
   const totalRevenueQuery = `
   SELECT SUM("unitPrice" * "quantity") as "totalRevenue"
   FROM "SaleProduct"`;
@@ -56,5 +94,6 @@ export const getDashboard = async (): Promise<DashboardDto> => {
     totalSales: totalSales,
     totalStock: Number(totalStock._sum.stock ?? 0),
     totalProducts: totalProducts,
+    totalLast14DaysRevenue: totalLast14DaysRevenue,
   };
 };
