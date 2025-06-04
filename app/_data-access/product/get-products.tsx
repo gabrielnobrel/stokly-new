bd2f44c04998dabcf3a0cf263b38081f015976f2
gabrielnobrel <gabriel_nobresantos@hotmail.com>
2025-06-04 10:52:44 -0300

    add type in get-products

--- app/_data-access/product/get-products.tsx
+++ app/_data-access/product/get-products.tsx
@@ -5,7 +5,8 @@ import { db } from "@/app/_lib/prisma";
 
 export type ProductStatusDto = "IN_STOCK" | "OUT_OF_STOCK";
 
-export interface ProductDto extends Product {
+export interface ProductDto extends Omit<Product, "price"> {
+  price: number;
   status: ProductStatusDto;
 }
 
@@ -14,6 +15,7 @@ export const getProducts = async (): Promise<ProductDto[]> => {
   const products = await db.product.findMany({});
   return products.map((product) => ({
     ...product,
+    price: Number(product.price),
     status: product.stock > 0 ? "IN_STOCK" : "OUT_OF_STOCK",
   }));
 };
